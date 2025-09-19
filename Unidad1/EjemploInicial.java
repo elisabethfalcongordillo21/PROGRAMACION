b385fd308410f58684179c3e36e9b26cef1c579c
elisabethfalcongordillo21 <elisabethfalcongordillo21@gmail.com>
2025-09-19 14:57:06 +0200

    EjemploInicial

---
+++ Unidad1/EjemploInicial.java
@@ -0,0 +1,40 @@
+package Unidad1;
+
+public class EjemploInicial {
+
+    public static void main(String args []){
+        
+        /**
+        * 
+        * 
+        */
+
+        /*
+         * dddd
+         * ddd
+         * ddd
+         */
+        
+        
+         //Para definir variables ponemos el tipo de dato que va a guardar
+        //Y despues el nombre de la variable que lo definamos nosotros
+            int edad;
+            edad = 19;
+
+            //Variabled de tipo decimal
+            float peso = 215.5f;
+            double pi = 3.149237856;
+
+            //Cadenas de texto y Caracteres
+            String nombre = "Ruben Vallejo";
+
+            char letrainicial = 'J';
+
+            //Boolean
+            boolean esPadre = true;
+            
+
+            //Aqui se escribe el código del programa
+        System.out.print("Bienvenido al curso 2526 de Programación!!");   
+    }
+}

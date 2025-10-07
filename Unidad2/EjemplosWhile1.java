2537d7ef364468466f1f9833661a80e466f204e5
elisabethfalcongordillo21 <elisabethfalcongordillo21@gmail.com>
2025-10-07 13:00:43 +0200

    ejemplowhile2

--- Unidad2/EjemplosWhile1.java
+++ Unidad2/EjemplosWhile1.java
@@ -28,32 +28,51 @@ public class EjemplosWhile1 {
         i++;
   }  
 
+/*Ejerciciov2: leer las edades de 10 personas, que deben de estar entre 18 y 65, 
+se debe sacar por pantalla la edad y si esta jubilado o si es menor de edad y calcular la media final
+*/
+
   int edad=0;
   int sumaEdad=0;
   i=1;
+  int cantidadPersonas=0;
+   String mensaje="";
 
   //Quiero que se repita 10 veces
   while (i<=10) {
+
+    mensaje=""; 
+    //Genero la edad aleatoria entre 1 y 100
+
+    edad = (int)(Math.random()*100)+1;
     
+    if (edad < 18) 
 
-    //Genero la edad aleatoria
-
-    edad =18 + (int)(Math.random()*48);
+       mensaje="La persona es menor de edad, su edad es ";      
+     else if (edad > 65 )
+        mensaje="La persona es jubilada, su edad es " + edad + ". ";
+    else {
+       
+        sumaEdad= sumaEdad + edad;
+        //incrementamos la cantidad de las personas entre 18 y 65 para hacer la media 
+        cantidadPersonas= cantidadPersonas + 1;
+  
+    }
+     //mostramos la edad del personaje
+        mensaje= "La edad de la persona " + i + " es: " + edad;
+     //Pasamos a la siguiente persona
+     i++;
+     
     
-    //mostramos la edad del personaje
-
-    System.out.println("La edad de la persona " + i + " es: " + edad);
-
-    //Pasamos a la siguiente persona
-
-    i++;
-
-    sumaEdad= sumaEdad + edad;
 }
 
     //Calculamos la media
-    sumaEdad = sumaEdad/10;
+   if (cantidadPersonas !=0) 
+     sumaEdad = sumaEdad/10;
     System.out.println("La Media es " + sumaEdad);
+
+   
+   
 
 
 

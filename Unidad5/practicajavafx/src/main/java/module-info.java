dd7ad86a78dda61d35864d43850a82859e05117e
elisabethfalcongordillo21 <elisabethfalcongordillo21@gmail.com>
2026-05-05 17:54:24 +0200

    aaa

--- Unidad5/practicajavafx/src/main/java/module-info.java
+++ Unidad5/practicajavafx/src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module com.practicajavafx {
     requires javafx.controls;
+    requires javafx.graphics;
+   requires java.sql;
+   requires io.github.cdimascio.dotenv.java;
     exports com.practicajavafx;
 }
